0db8d6f3c1d8f30ca204b69a8e86b1775e67825b
kazij317-code <kazij317@gmail.com>
2026-04-15 12:21:46 +0600

    Project initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="bg-blue-300 text-red-600 text-center">
+      Hello Start My Project
+    </div>
+   
+  );
+}
+
+export default App;
