6fce50d67bef6e5b16cc599bc9ef5360a4bd576d
kazij317-code <kazij317@gmail.com>
2026-04-15 22:46:16 +0600

    Added Toaster, 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
-import './App.css'
-
-import Navbar from "../../New folder/src/components/Navbar";
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import Footer from './components/Footer';
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import toast, { Toaster } from 'react-hot-toast';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import FriendDetails from "./pages/FriendDetails";
 import Timeline from "./pages/Timeline";
+import './App.css'
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
-
+    // <BrowserRouter>
     <HashRouter>
-      <div>
+      <div className="min-h-screen bg-[#E9E9E9] font-sans text-gray-900">
+        <Toaster position="top-right" />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/friend/:id" element={<FriendDetails />} />
           <Route path="/timeline" element={<Timeline />} />
+          <Route path="*" element={<div className="h-screen flex flex-col items-center justify-center">
+            <h1 className="text-6xl font-bold text-emerald-900">404</h1>
+            <p className="text-xl text-gray-500 mt-4">Oops! This page doesn't exist.</p>
+          </div>} />
         </Routes>
         <Footer />
-
       </div>
-
     </HashRouter>
+    // </BrowserRouter>
   );
 }
 
